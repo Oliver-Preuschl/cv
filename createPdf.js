40e6c983ffcad63a81842bfb98fb7d99508f37f7
Oliver-Preuschl <olli@preuschl.com>
2021-04-08 23:20:20 +0200

    Update: Layout

---
+++ createPdf.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://127.0.0.1:4000/', {
+    waitUntil: 'networkidle2',
+  });
+  await page.pdf({ path: 'cv.pdf', format: 'a4' });
+
+  await browser.close();
+})();
